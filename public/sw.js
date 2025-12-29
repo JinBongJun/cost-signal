ce7f46bb90a4a57990f90490d1460dcb2ce47c90
Jin Bong Jun <bongjun0289@gmail.com>
2025-12-29 20:31:15 +0900

    Add push notification handlers to service worker

--- public/sw.js
+++ public/sw.js
@@ -91,3 +91,77 @@ define(['./workbox-7144475a'], (function (workbox) { 'use strict';
 
 }));
 //# sourceMappingURL=sw.js.map
+
+// Push notification event handlers (injected by add-push-handlers.js)
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push event received:', event);
+
+  let notificationData = {
+    title: 'Cost Signal',
+    body: 'You have a new notification',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    tag: 'cost-signal-notification',
+    requireInteraction: false,
+    data: {
+      url: '/',
+    },
+  };
+
+  if (event.data) {
+    try {
+      const data = event.data.json();
+      notificationData = {
+        title: data.title || notificationData.title,
+        body: data.body || notificationData.body,
+        icon: data.icon || notificationData.icon,
+        badge: data.badge || notificationData.badge,
+        tag: data.tag || notificationData.tag,
+        requireInteraction: data.requireInteraction || false,
+        data: data.data || notificationData.data,
+      };
+    } catch (e) {
+      console.error('[Service Worker] Error parsing push data:', e);
+    }
+  }
+
+  const promiseChain = self.registration.showNotification(
+    notificationData.title,
+    {
+      body: notificationData.body,
+      icon: notificationData.icon,
+      badge: notificationData.badge,
+      tag: notificationData.tag,
+      requireInteraction: notificationData.requireInteraction,
+      data: notificationData.data,
+    }
+  );
+
+  event.waitUntil(promiseChain);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click received:', event);
+  event.notification.close();
+
+  const urlToOpen = event.notification.data?.url || '/';
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window',
+        includeUncontrolled: true,
+      })
+      .then(function(clientList) {
+        for (let i = 0; i < clientList.length; i++) {
+          const client = clientList[i];
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+  );
+});
