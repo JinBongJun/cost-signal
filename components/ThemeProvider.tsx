30169a0fe535fafa90aebb2b5502be94a84ef26b
Jin Bong Jun <bongjun0289@gmail.com>
2025-12-31 11:31:25 +0900

    Fix ThemeProvider types using ComponentProps

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,15 +1,9 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ReactNode } from 'react';
+import { type ComponentProps } from 'react';
 
-interface ThemeProviderProps {
-  children: ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
-}
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
